687d62721289713d8cde9208042efa38a7fc59ff
Darío Cáceres <dignaciocaceres@gmail.com>
2020-04-16 17:30:47 -0400

    Created Todo Model. Closed issue #2

--- notesTDD/todos/tests.py
+++ notesTDD/todos/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from todos.models import *
 # Create your tests here.
+
+class TestTodo(TestCase):
+
+    # Dado el Contexto (Given)
+    def setUp(self):        
+        Todo.objects.create(name="todo1")
+
+    def test_agregar_tarea(self):
+        Todo.objects.create(name='todo2')
+        self.assertEqual(Todo.objects.get(name="todo2").name ,'todo2')
